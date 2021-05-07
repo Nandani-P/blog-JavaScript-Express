0de789ca8406658af09beeb651809ce414697ce6
Nandani <np744@scarletmail.rutgers.edu>
2021-05-07 16:42:12 -0400

    Project completed

---
+++ src/build/precache-manifest.6706c2408d0094fe70c0158331874b6a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "aa25f4b3dde5ae3d005d",
+    "url": "/static/css/main.717234c2.chunk.css"
+  },
+  {
+    "revision": "aa25f4b3dde5ae3d005d",
+    "url": "/static/js/main.e80208d1.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "6fc5404483c4e57544a1",
+    "url": "/static/js/2.bbdd57f0.chunk.js"
+  },
+  {
+    "revision": "5f05d96569b247ff0c4b8cbb6a9d68da",
+    "url": "/index.html"
+  }
+];
